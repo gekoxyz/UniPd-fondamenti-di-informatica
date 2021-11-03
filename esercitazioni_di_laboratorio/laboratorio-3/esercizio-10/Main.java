fe0718832abfdc97ad785592a8d9ee1f631700a9
Matteo <mattegaliz@gmail.com>
2021-11-03 18:43:38 +0100

    fatto fino a esercizio-9 laboratorio-3

--- esercitazioni_di_laboratorio/laboratorio-3/esercizio-10/Main.java
+++ esercitazioni_di_laboratorio/laboratorio-3/esercizio-10/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /*
 Scrivere il programma PrintTimeInterval2.java, modificando il programma PrintTimeInterval visto in 
 precedenza in modo che funzioni correttamente anche se il secondo orario è inferiore al primo (cioè 
@@ -8,5 +9,36 @@ Inserire il secondo orario: 0900
 Tempo trascorso: 15 ore e 30 minuti
 */
 public class Main {
-
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String time1 = "", time2 = "";
+        System.out.println("Inserisci il primo orario: ");
+        time1 = scan.next();
+        System.out.println("Inserisci il secondo orario: ");
+        time2 = scan.next();
+        scan.close();
+        int time1Hours = 0, time1Minutes = 0, time2Hours = 0, time2Minutes = 0;
+        try {
+            int tempTime1 = Integer.parseInt(time1);
+            int tempTime2 = Integer.parseInt(time2);
+            if (tempTime1 > tempTime2) {
+                
+            }
+            time1Hours = Integer.parseInt(time1.substring(0, 2));
+            time1Minutes = Integer.parseInt(time1.substring(2, 4));
+            time2Hours = Integer.parseInt(time2.substring(0, 2));
+            time2Minutes = Integer.parseInt(time2.substring(2, 4));
+        } catch (NumberFormatException e) {
+            System.out.println("Hai inserito uno degli orari in modo sbagliato");
+        }
+        int hoursDifference = time2Hours - time1Hours;
+        int minutesDifference = 0;
+        if (time2Minutes < time1Minutes) {
+            hoursDifference--;
+            minutesDifference = time2Minutes + 60 - time1Minutes;
+        } else {
+            minutesDifference = time2Minutes - time1Minutes;
+        }
+        System.out.println("Tempo trascorso: " + hoursDifference + " ore e " + minutesDifference + " minuti");
+    }
 }
--- esercitazioni_di_laboratorio/laboratorio-3/esercizio-8/Main.java
+++ esercitazioni_di_laboratorio/laboratorio-3/esercizio-8/Main.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 /*
 Scrivere un programma che
 
-- riceva in ingresso una stringa rappresentante un aggettivo qualificativo maschile o femminile (terminante con il carattere 'o' o con il carattere 'a')
+- riceva in ingresso una stringa rappresentante un aggettivo qualificativo maschile o femminile 
+    (terminante con il carattere 'o' o con il carattere 'a')
 - stampi a standard output l'aggettivo inserito, con solo il primo carattere maiuscolo,
 - stampi a standard output la forma diminutiva (-ino / -ina) dell'aggettivo inserito
 - stampi a standard output il grado superlativo assoluto (-issimo / -issima) dell'aggettivo inserito
@@ -13,8 +16,26 @@ Se invece viene inserita la stringa "cara", il programma visualizzera` l'output
 Aggettivo inserito:    Cara
 Forma diminutiva:      Carina
 Superlativo assoluto:  Carissima
-Suggerimento: studiare la documentazione della classe String e trovare i metodi utili a risolvere il problema. 
 */
 public class Main {
-
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String firstCharUp, diminutive, superlative;
+        System.out.println("Inserisci un aggettivo");
+        String adjective = scan.next();
+        scan.close();
+        int penultimateCharIndex = adjective.length() - 1;
+        firstCharUp = adjective.substring(0, 1).toUpperCase() + adjective.substring(1);
+        if (adjective.charAt(adjective.length() - 1) == 'o') {
+            diminutive = firstCharUp.substring(0, penultimateCharIndex) + "ino";
+            superlative = firstCharUp.substring(0, penultimateCharIndex) + "issimo";
+        } else {
+            diminutive = firstCharUp.substring(0, penultimateCharIndex) + "ina";
+            superlative = firstCharUp.substring(0, penultimateCharIndex) + "issima";
+        }
+        System.out.println("Aggettivo: " + adjective);
+        System.out.println("Con prima lettera maiuscola: " + firstCharUp);
+        System.out.println("Diminutivo: " + diminutive);
+        System.out.println("Superlativo: " + superlative);
+    }
 }
