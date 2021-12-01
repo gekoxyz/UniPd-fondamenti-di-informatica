4690b0366e72e20b417cb16fb90083e159648f84
Matteo <mattegaliz@gmail.com>
2021-12-01 16:39:50 +0100

    aggiunto il testo dell'esercizio-6

---
+++ esercitazioni_di_laboratorio/laboratorio-6/esercizio-6/MyComplex.java
@@ -0,0 +1,17 @@
+/*
+Si scriva la classe MyComplex, che rappresenta un numero complesso, la cui interfaccia pubblica 
+è specificata nel file MyComplex.java disponibile tra i file messi a disposizione per il laboratorio 
+(e contiene come commenti le uniche nozioni sui numeri complessi che e` necessario conoscere ai fini 
+di questo esercizio).
+Collaudare la classe MyComplex con la classe di prova MyComplexTester, disponibile tra i file messi 
+a disposizione per il laboratorio. Il programma riceve due numeri complessi da standard input, uno per 
+riga, nel formato "x y ", dove x (parte reale) e y (parte immaginaria) sono separate dal carattere 
+spazio ' '. Fornisce a standard output somma, sottrazione, prodotto, divisione, elementi inversi e 
+coniugati dei numeri. Provare con numeri semplici, in modo da verificare i risultati ottenuti.
+Effettuare il collaudo in due modi: ciascuna delle due classi in un file .java ad essa omonimo (con 
+entrambe le classi public), oppure entrambe le classi in un solo file MyComplexTester.java (con 
+MyComplexTester public e MyComplex no)
+*/
+public class MyComplex {
+   public MyComplex(){} 
+}
