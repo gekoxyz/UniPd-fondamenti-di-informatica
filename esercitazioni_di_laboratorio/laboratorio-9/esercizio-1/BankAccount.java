3487adbe738ffb0d6ffc7e0190024b30c5f1b763
Gekoxyz <mattegaliz@gmail.com>
2021-12-14 16:58:06 +0100

    fatto fino a esercizio-1 in laboratorio-9

---
+++ esercitazioni_di_laboratorio/laboratorio-9/esercizio-1/BankAccount.java
@@ -0,0 +1,49 @@
+public class BankAccount {
+    public BankAccount() {
+        balance = 0;
+    }
+
+    public BankAccount(double initialBalance) {
+        deposit(initialBalance);
+    }
+
+    public void deposit(double amount) {
+        if (amount <= 0)
+            throw new IllegalArgumentException();
+        balance = balance + amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount > balance || amount <= 0)
+            throw new IllegalArgumentException();
+        balance = balance - amount;
+    }
+
+    public void transfer(double amount, BankAccount other) {
+        withdraw(amount);
+        other.deposit(amount);
+    }
+
+    // ------- metodi di accesso --------
+
+    public double getBalance() {
+        return balance;
+    }
+
+    // ------ metodi di Object da sovrascrivere ---------
+    // ......... toString e equals ......................
+
+    @Override
+    public boolean equals(Object toCompare) {
+        BankAccount otherAccount = (BankAccount) toCompare;
+        return balance == otherAccount.getBalance();
+    }
+
+    @Override
+    public String toString() {
+        return "balance: " + balance;
+    }
+    // -------- campi di esemplare ---------
+
+    private double balance;
+}
---
+++ esercitazioni_di_laboratorio/laboratorio-9/esercizio-1/SavingsAccount.java
@@ -0,0 +1,40 @@
+public class SavingsAccount extends BankAccount {
+    public SavingsAccount(double rate) {
+        super(); // costruttore della superclasse
+        interestRate = rate;
+    }
+
+    public SavingsAccount(double initialBalance, double rate) {
+        super(initialBalance); // costruttore della superclasse
+        interestRate = rate;
+    }
+
+    // accredita gli interessi al termine del mese. Attenzione: usa il metodo
+    // deposit della superclasse, altrimenti verrebbe addebitata la penale FEE
+    public void addInterest() // NUOVO METODO
+    {
+        super.deposit(getBalance() * interestRate / 100);
+    }
+
+    // ------- metodi di accesso --------
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    // ------ metodi di Object sovrascritti ---------
+    // ........... toString, equals .................
+    @Override
+    public boolean equals(Object toCompare) {
+        SavingsAccount otherAccount = (SavingsAccount) toCompare;
+        return interestRate == otherAccount.interestRate;
+    }
+
+    @Override
+    public String toString() {
+        return "interest rate: " + interestRate;
+    }
+    // -------- nuovi campi di esemplare ----------------
+
+    private double interestRate;
+}
